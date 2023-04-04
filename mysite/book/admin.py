bc9e5e0f0194cdd2adb7584f4091c7bf4dfb46e0
Slimeding <aslimeding@gmail.com>
2023-04-04 13:41:40 +0800

    four_tables

--- mysite/book/admin.py
+++ mysite/book/admin.py
@@ -1,19 +1,22 @@
 from django.contrib import admin
 from book.models import BookClass,BookInfo
+from book.models import BookISBN,AutorInfo
+# Register your models here.
+
 
 class BookInfoAdmin(admin.ModelAdmin):
     list_display=('id',"name")
 class BookClassAdmin(admin.ModelAdmin):
     list_display=('id',"name")
-# class BookISBNAdmin(admin.ModelAdmin):
-#     list_display = ("id","isbn")
-# class AutorInfoAdmin(admin.ModelAdmin):
-#     list_display = ("id","name")
+class BookISBNAdmin(admin.ModelAdmin):
+    list_display = ("id","isbn")
+class AutorInfoAdmin(admin.ModelAdmin):
+    list_display = ("id","name")
 
 
 admin.site.register(BookClass,BookClassAdmin)
 admin.site.register(BookInfo,BookInfoAdmin)
-# admin.site.register(BookISBN,BookISBNAdmin)
-# admin.site.register(AutorInfo,AutorInfoAdmin)
+admin.site.register(BookISBN,BookISBNAdmin)
+admin.site.register(AutorInfo,AutorInfoAdmin)
 admin.site.site_header="管理后台"
 admin.site.site_title="后台管理系统"
