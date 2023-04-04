fd134a73defe1f8a374fd4c081ca51cdc47d7b3c
Slimeding <aslimeding@gmail.com>
2023-04-04 13:56:18 +0800

    login

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from users.views import index
+from users import views
 
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', index),
+    path('login/', views.login),
 ]
--- mysite/users/views.py
+++ mysite/users/views.py
@@ -2,5 +2,26 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+
+# 1.hello world
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+# 2.模板
+def index(request):
+    return render(request, 'login.html')
+def login(request):
+    if request.method == 'GET':
+        name = request.POST.get('name')
+        pwd = request.POST.get('pwd')
+        context = {'name': name, 'pwd': pwd}
+        print(str(context))
+        return HttpResponse('登录成功')
+    else:
+        name = request.POST.get('name')
+        pwd = request.POST.get('pwd')
+        context = {'name': name, 'pwd': pwd}
+        print(str(context))
+        return render(request, 'login.html')
+        # return redirect('https://360.com')
+
