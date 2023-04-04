048cd0aa004d9052fd71ae0442f4f5c1f7942577
Slimeding <aslimeding@gmail.com>
2023-04-04 13:32:39 +0800

    BookClass,Bookinfo

--- mysite/book/admin.py
+++ mysite/book/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from book.models import BookClass,BookInfo
 
-# Register your models here.
+class BookInfoAdmin(admin.ModelAdmin):
+    list_display=('id',"name")
+class BookClassAdmin(admin.ModelAdmin):
+    list_display=('id',"name")
+# class BookISBNAdmin(admin.ModelAdmin):
+#     list_display = ("id","isbn")
+# class AutorInfoAdmin(admin.ModelAdmin):
+#     list_display = ("id","name")
+
+
+admin.site.register(BookClass,BookClassAdmin)
+admin.site.register(BookInfo,BookInfoAdmin)
+# admin.site.register(BookISBN,BookISBNAdmin)
+# admin.site.register(AutorInfo,AutorInfoAdmin)
+admin.site.site_header="管理后台"
+admin.site.site_title="后台管理系统"
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from users import views
 from users.views import index
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
